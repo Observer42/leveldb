d935fa9488e6bdf4d4214c49085cbf2bcaab8a65
Yisheng XU <yishengxu@outlook.com>
2018-05-07 00:43:38 +0800

    add leveldb_example.cc

---
+++ example/leveldb_example.cc
@@ -0,0 +1,24 @@
+#include "leveldb/db.h"
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+using namespace leveldb;
+
+int main() {
+    leveldb::DB *db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options, "testdb", &db);
+    assert(status.ok());
+
+    status = db->Put(WriteOptions(), "key", "Hello LevelDB!");
+    assert(status.ok());
+    string res;
+    status = db->Get(ReadOptions(), "key", &res);
+    assert(status.ok());
+    cout << res << endl;
+
+    delete db;
+    return 0;
+}
